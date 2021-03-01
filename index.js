ca96301ae5ae33f02c5a8875b9b7fe38d933b192
milenajovanovic994 <milenche.jovanovic@gmail.com>
2021-03-01 10:29:39 +0100

    Connected my app with MongoDB, corrected hashing password(now CryptoJS is used),adapted my code to work with MOngoDB

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-// hDlCq6zZQxi3wQmY
-
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import cors from 'cors'
-// import CryptoJS from 'crypto-js'
-// import md5 from 'crypto-js/md5.js'
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import CryptoJS from 'crypto-js'
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const recipeSchema = new mongoose.Schema({
+    recipe: String,
+    author: String,
+    title: String,
+    ingredients: Array,
+    dishType: String,
+    img: String
+})
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+const User = mongoose.model('User', userSchema)
 
 const app = express()
-
-import bcrypt from 'bcrypt'
-// const bcrypt = require('bcrypt')
-// const CryptoJS = require("crypto-js")
 
 app.use(express.json())
 app.use(cors())
@@ -40,145 +58,229 @@ const USERS = '/users'
 
 const LOGIN = '/login'
 
-let recipes = [
-    {
-        id: 1,
-        recipe: 'nesto1',
-        author: 'Chef Gagi',
-        title: 'ime1',
-        ingredients: ['jabuka','narandza','kruska','sljiva','secer'],
-        dishType: 'Sweets',
-        img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997883/samples/food/spices.jpg'
-    },
-    {
-        id: 2,
-        recipe: 'nesto2',
-        author: 'Chef Gagi',
-        title: 'ime2',
-        ingredients: ['krompir','paradajz','sargarepa','ulje','so'],
-        dishType: 'Baked goods',
-        img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997876/samples/food/pot-mussels.jpg'
-    },
-    {
-        id: 3,
-        recipe: 'nesto3',
-        author: 'Chef Gagi',
-        title: 'ime3',
-        ingredients: ['avokado','brasno','voda'],
-        dishType: 'Fruit dish',
-        img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997875/samples/food/fish-vegetables.jpg'
-    },
-    {
-        id: 4,
-        recipe: 'nesto4',
-        author: 'Chef Gagi',
-        title: 'ime4',
-        ingredients: ['cokolada','mleko','secer','gustin'],
-        dishType: 'Sweets',
-        img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997874/samples/food/dessert.jpg'
-    },
-    {
-        id: 5,
-        recipe: 'nesto5',
-        author: 'Chef Gagi',
-        title: 'ime5',
-        ingredients: ['jabuka','narandza','sljiva','secer'],
-        dishType: 'Sweets',
-        img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997870/sample.jpg'
-    }
-]
+// let recipes = [
+//     {
+//         id: 1,
+//         recipe: 'nesto1',
+//         author: 'Chef Gagi',
+//         title: 'ime1',
+//         ingredients: ['jabuka','narandza','kruska','sljiva','secer'],
+//         dishType: 'Sweets',
+//         img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997883/samples/food/spices.jpg'
+//     },
+//     {
+//         id: 2,
+//         recipe: 'nesto2',
+//         author: 'Chef Gagi',
+//         title: 'ime2',
+//         ingredients: ['krompir','paradajz','sargarepa','ulje','so'],
+//         dishType: 'Baked goods',
+//         img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997876/samples/food/pot-mussels.jpg'
+//     },
+//     {
+//         id: 3,
+//         recipe: 'nesto3',
+//         author: 'Chef Gagi',
+//         title: 'ime3',
+//         ingredients: ['avokado','brasno','voda'],
+//         dishType: 'Fruit dish',
+//         img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997875/samples/food/fish-vegetables.jpg'
+//     },
+//     {
+//         id: 4,
+//         recipe: 'nesto4',
+//         author: 'Chef Gagi',
+//         title: 'ime4',
+//         ingredients: ['cokolada','mleko','secer','gustin'],
+//         dishType: 'Sweets',
+//         img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997874/samples/food/dessert.jpg'
+//     },
+//     {
+//         id: 5,
+//         recipe: 'nesto5',
+//         author: 'Chef Gagi',
+//         title: 'ime5',
+//         ingredients: ['jabuka','narandza','sljiva','secer'],
+//         dishType: 'Sweets',
+//         img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997870/sample.jpg'
+//     }
+// ]
 
-let users = [
-    {
-        id: 1,
-        username: 'Milena',
-        password: '12345abcd',
-        email: 'nesto@gmail.com'
-    },
-    {
-        id: 2,
-        username: 'Pera',
-        password: 'abcd12345',
-        email: 'nesto2@gmail.com'
-    }
-]
+// let users = [
+//     {
+//         id: 1,
+//         username: 'Milena',
+//         password: '12345abcd',
+//         email: 'nesto@gmail.com'
+//     },
+//     {
+//         id: 2,
+//         username: 'Pera',
+//         password: 'abcd12345',
+//         email: 'nesto2@gmail.com'
+//     }
+// ]
 
 
 app.get(RECIPES, (_, res) => {
-    res.status(200).json(recipes)
+
+    Recipe.find({}).then(result => {
+        res.json(result)
+    })
+
+    // res.status(200).json(recipes)
 })
 
 app.get(USERS, (_, res) => {
-    res.status(200).json(users)
+
+    User.find({}).then(result => {
+        res.json(result)
+    })
+
+    // res.status(200).json(users)
 })
 
 
 app.get(`${RECIPES}/:id`, (req, res) => {
     const id = req.params.id
-    const recipe = recipes.find(r => r.id == id)
+    // const recipe = recipes.find(r => r.id == id)
 
-    if (recipe)
-        res.json(recipe)
-    else res.status(404).end()
+    Recipe.findById(id).then(recipe => {
+        if (recipe) res.json(recipe)
+        else res.status(404).end()
+    })
+        .catch(error => {
+            console.log(error)
+            res.status(500).end()
+        })
+
+    // if (recipe)
+    //     res.json(recipe)
+    // else res.status(404).end()
 })
 
 app.get(`${USERS}/:id`, (req, res) => {
     const id = req.params.id
-    const user = users.find(u => u.id == id)
+    // const user = users.find(u => u.id == id)
 
-    if (user)
-        res.json(user)
-    else res.status(404).end()
+    // if (user)
+    //     res.json(user)
+    // else res.status(404).end()
+
+    User.findById(id).then(user => {
+        if (user) res.json(user)
+        else res.status(404).end()
+    })
+        .catch(error => {
+            console.log(error)
+            res.status(500).end()
+        })
+
 })
 
 
 app.post(RECIPES, (req, res) => {
 
-    const newRecipe = req.body
+    // const newRecipe = req.body
 
-    if (newRecipe.recipe.trim().length === 0 || newRecipe.author.trim().length === 0 || newRecipe.title.trim().length === 0) {
-        res.status(400).json({ error: 'All fields must be filled in.' })
-        return
-    }
+    // if (newRecipe.recipe.trim().length === 0 || newRecipe.author.trim().length === 0 || newRecipe.title.trim().length === 0) {
+    //     res.status(400).json({ error: 'All fields must be filled in.' })
+    //     return
+    // }
 
-    const id = recipes.length > 0 ?
-        Math.max(...recipes.map(recipe => recipe.id)) + 1
-        : 1
-    newRecipe.id = id
+    // const id = recipes.length > 0 ?
+    //     Math.max(...recipes.map(recipe => recipe.id)) + 1
+    //     : 1
+    // newRecipe.id = id
 
-    recipes.push(newRecipe)
+    // recipes.push(newRecipe)
 
-    res.status(201).json(newRecipe)
+    // res.status(201).json(newRecipe)
+
+    const recipe = new Recipe({
+        recipe: req.body.recipe,
+        author: req.body.author,
+        title: req.body.title,
+        ingredients: req.body.ingredients,
+        dishType: req.body.dishType,
+        img: req.body.img
+    })
+
+    recipe.save().then(result => {
+        res.json(result)
+    })
 })
 
 
 
-app.post(USERS, async (req, res) => {
+app.post(USERS, (req, res) => {
 
 
-    if(isUser(users, req.body.username, req.body.email)){
-        res.status(400).json({ error: 'User already exists with this username or email.' })
-        return
-    }
-    else{
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // if (isUser(users, req.body.username, req.body.email)) {
+    //     res.status(400).json({ error: 'User already exists with this username or email.' })
+    //     return
+    // }
+    // else {
 
-            const newUser = req.body
+    // const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
-            newUser.password = hashedPassword
-
-            const id = users.length > 0 ?
-                Math.max(...users.map(user => user.id)) + 1
-                : 1
-            newUser.id = id
-
-            users.push(newUser)
-            res.status(201).json(newUser)
-        } catch {
-            res.status(500).send()
+    User.findOne({ username: req.body.username }).then(result => {
+        console.log(result)
+        if (result) {
+            res.status(400).json({ error: 'User already exists with this username or email.' })
+            return
         }
-    }
+        else {
+
+                req.body.password = CryptoJS.MD5(req.body.password).toString()
+
+
+                const user = new User({
+                    username: req.body.username,
+                    password: req.body.password,
+                    email: req.body.email
+                })
+
+                user.save().then(result => {
+                    res.json(result)
+                })
+        }
+
+    }).catch(error => {
+        console.log(error)
+        res.status(404).end()
+    })
+
+
+
+    // try {
+    //     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    //     const user = new User({
+    //         username: req.body.username,
+    //         password: hashedPassword,
+    //         email: req.body.email
+    //     })
+
+    //     user.save().then(result => {
+    //         res.json(result)
+    //     })
+
+    // const newUser = req.body
+
+    // newUser.password = hashedPassword
+
+    // const id = users.length > 0 ?
+    //     Math.max(...users.map(user => user.id)) + 1
+    //     : 1
+    // newUser.id = id
+
+    // users.push(newUser)
+    // res.status(201).json(newUser)
+    // } catch {
+    //     res.status(500).send()
+    // }
+    // }
 
     // if (!isUser(users, req.body.username, req.body.email)) {
 
@@ -210,22 +312,43 @@ app.post(USERS, async (req, res) => {
 })
 
 app.post(`${USERS}${LOGIN}`, async (req, res) => {
-    const user = users.find(user => user.username === req.body.username)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success')
-        } else {
-            res.send('Not Allowed')
-            return
+    // const user = users.find(user => user.username === req.body.username)
 
-            // res.status(404).send('Not Allowed')
+    User.findOne({ username: req.body.username, email: req.body.email }).then(result => {
+        if(result){
+            console.log(result) 
+                if (CryptoJS.MD5(req.body.password).toString() === result.password) {
+                    res.send('Success')
+                } else {
+                    res.send('Not Allowed')
+                    return
+        
+                    // res.status(404).send('Not Allowed')
+                }    
         }
-    } catch {
-        res.status(500).send()
-    }
+        else{
+            return res.status(400).send('Cannot find user')
+        }
+    }).catch(error => {
+        console.log(error)
+        res.status(500).end()
+    })
+
+    // if (user == null) {
+    //     return res.status(400).send('Cannot find user')
+    // }
+    // try {
+    //     if (await bcrypt.compare(req.body.password, user.password)) {
+    //         res.send('Success')
+    //     } else {
+    //         res.send('Not Allowed')
+    //         return
+
+    //         // res.status(404).send('Not Allowed')
+    //     }
+    // } catch {
+    //     res.status(500).send()
+    // }
 
 })
 
---
+++ tmp.js
@@ -0,0 +1,51 @@
+// bHCSTmj8ETYx4yba
+
+// mongodb+srv://milenajovanovic994:bHCSTmj8ETYx4yba@cluster0.0hhjj.mongodb.net/chefGagiApp?retryWrites=true&w=majority
+
+import mongoose from 'mongoose'
+
+const url = 'mongodb+srv://milenajovanovic994:bHCSTmj8ETYx4yba@cluster0.0hhjj.mongodb.net/chefGagiApp?retryWrites=true&w=majority'
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const recipeSchema = new mongoose.Schema({
+    recipe: String,
+    author: String,
+    title: String,
+    ingredients: Array,  // array
+    dishType: String,
+    img: String
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+// const recipe = new Recipe({
+//     recipe: 'bla bla bla bla bla',
+//     author: 'Neko proba',
+//     title: 'Ime proba',
+//     ingredients: ['proba1', 'proba2'],
+//     dishType: 'Tip proba',
+//     img: 'https://res.cloudinary.com/milenajovanovic994/image/upload/v1613997883/samples/food/spices.jpg'
+// })
+
+// recipe.save().then(res => {
+//     console.log('RECIPE снимљен')
+//     mongoose.connection.close()
+// })
+
+// Recipe.find({}).then(res => {
+//     console.log(res)
+//     mongoose.connection.close()
+// })
+
+// -----------------------------------
+
+// const userSchema = new mongoose.Schema({
+//     username: String,
+//     password: String,
+//     email: String
+// })
+
+// const User = mongoose.model('User', userSchema)
+
+// -----------------------------------------
